3ec1e869390234f1cf0cb4146cf58783ba6cecc3
michael-fei <michael-fei@users.noreply.github.com>
2017-03-09 19:14:38 -0500

    Add Initial class which implements Product

--- ts/product.ts
+++ ts/product.ts
@@ -6,6 +6,11 @@ interface Product {
     category?: ProductCategory
 }
 
+class Initial implements Product {
+    name: string = "Please select a product";
+    price: number = 0;
+}
+
 class CocaCola implements Product {
     name: string = "Coca-Cola";
     price: number = 2.30;
