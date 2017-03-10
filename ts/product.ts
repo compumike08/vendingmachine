ce6dc490afe29b41c32cac264071149fe3cfe32a
michael-fei <michael-fei@users.noreply.github.com>
2017-03-09 19:14:13 -0500

    Add Product interface and have CocaCola class implement it

--- ts/product.ts
+++ ts/product.ts
@@ -1,6 +1,12 @@
 /// <reference path="productCategory.ts" />
 
-class CocaCola {
+interface Product {
+    name: string;
+    price: number;
+    category?: ProductCategory
+}
+
+class CocaCola implements Product {
     name: string = "Coca-Cola";
     price: number = 2.30;
     category = new SodaCategory();
